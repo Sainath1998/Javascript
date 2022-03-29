29fa93c8d989823ac9b9dabeff33491f750b2008
Sainath <97929522+Sainath1998@users.noreply.github.com>
2022-03-29 11:26:57 +0530

    Add files via upload

--- undirected graph.js
+++ undirected graph.js
@@ -34,3 +34,37 @@ graph1.addEdge(2,3,8);
 graph1.addEdge(4,5,100);
 graph1.addEdge(1,5,88);
 console.log(graph1.edges)
+undirectedGraph.prototype.removeEdge = function(vertex1,vertex2)
+{
+    if(this.edges[vertex1] && this.edges[vertex1][vertex2] != undefined)
+    {
+        delete this.edges[vertex1][vertex2];
+    }
+    if(this.edges[vertex2] && this.edges[vertex2][vertex1]!=undefined)
+    {
+        delete this.edges[vertex2][vertex1];
+    }
+}
+undirectedGraph.prototype.removeVertex = function(vertex)
+{
+    for(var adjacentVertex in this.edges[vertex])
+    {
+        this.removeEdge(adjacentVertex,vertex)
+    }
+    delete this.edges[vertex];
+}
+
+var graph2 = new undirectedGraph();
+
+graph2.addVertex(1);
+graph2.addVertex(2);
+graph2.addEdge(1,2,1);
+console.log(graph2.edges);
+graph2.addVertex(3);
+graph2.addVertex(4);
+graph2.addVertex(5);
+graph2.addEdge(2,3,8);
+graph2.addEdge(4,5,100);
+graph2.addEdge(1,5,88);
+graph2.removeVertex(1);
+graph2.removeEdge(2,3)
